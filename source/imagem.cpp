7d86be3df4eff8bbcbdcbf427cbe627e9a925dee
Luan Guimarães <luan@live.in>
2015-03-31 23:11:21 -0300

    Implementando header da classe Imagem.

---
+++ source/imagem.cpp
@@ -0,0 +1,32 @@
+#include "imagem.hpp"
+#include "dimensoes.hpp"
+
+using namespace std;
+
+Imagem::Imagem(){
+	setIdentificador("NONE");
+	dimensoes.setDimensoes(0,0);
+}
+
+Imagem::Imagem(int identificador, int altura, int largura, int *pixels){
+	setIdentificador(identificador);
+	setPixels(pixels);
+	setDimensoes(altura, largura);
+}
+
+void Imagem::setIdentificador(string identificador){
+	this->identificador = identificador;
+}
+
+string Imagem::getIdentificador(){
+	return identificador;
+}
+
+void Imagem::setPixels(int *pixels){
+	this->pixels = pixels;
+}
+
+int * Imagem::getPixels(){
+	return pixels;
+}
+
