a241215587a2a8b4ff3228cd1e1646e59c8984c2
Luan Guimarães <luan@live.in>
2015-03-31 22:02:12 -0300

    Adicionando header da classe Imagem.

---
+++ include/imagem.hpp
@@ -0,0 +1,25 @@
+#ifndef IMAGEM_H
+#define IMAGEM_H
+
+#include "dimensoes.hpp"
+#include <string>
+
+
+
+class Imagem{
+	private:
+		string identificador;
+		Dimensoes dimensoes;
+		int *pixels;
+		
+	public:
+		Imagem();
+		Imagem(int identificador, int *pixels);
+		void setIdentificador(string identificador);
+		string getIdentificador();
+		void setPixels(int *pixels);
+		int *getPixels();
+		virtual FILE *abrirImagem(string caminho);	
+};
+
+#endif
