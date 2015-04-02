2c3617dddaf2dccc8973b22df867f59e89f0708b
Luan Guimarães <luan@live.in>
2015-04-02 03:17:29 -0300

    Implementando getters, setters e método de leitura de arquivos PGM da classe ImagemPGM.

---
+++ source/imagempgm.cpp
@@ -0,0 +1,106 @@
+#include "imagem.hpp"
+#include "imagempgm.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
+
+ImagemPGM::ImagemPGM(){
+
+}
+
+ImagemPGM::ImagemPGM(string caminho){
+	
+		
+}
+
+void ImagemPGM::abrirImagem(string caminho){
+
+	ifstream arquivo; //variável que armazena o arquivo da imagem
+	string id; //armazena o identificador da imagem, no caso da PGM (P5)
+	string comentario; //armazena os comentarios feitos
+	int x, y, counter; //x armazena quantas colunas há na imagem, y, quantas linhas e counter é um contador
+	int *pixels; // ponteiro que apontará para as posições alocadas para guardar os pixels
+	int nivelMaximoDeCinza; // armazena o nível máximo de cinza da imagem PGM
+	unsigned char reader; // necessário para ler corretamente o valor binário de 1byte lido do arquivo para cada pixel
+
+	try{
+		//tenta abrir a imagem com o caminho fornecido
+		arquivo.open(caminho.c_str());
+		
+		// verificando identidade da imagem
+		getline(arquivo, id);
+		
+		//compara a primeira linha com P5 para verificar
+		if(id.compare("P5") != 0){
+			cout << "Desculpe, abra um arquivo PGM!" <<endl;
+		}
+		else{
+			cout << "Arquivo PGM!" <<endl;
+
+			//lendo comentario
+			getline(arquivo, comentario);
+			cout << "Comentario: " <<comentario <<endl;
+			
+			//lendo dimensões e nível máximo de cinza da imagem
+			arquivo >> x;
+			arquivo >> y;
+			arquivo >> nivelMaximoDeCinza;
+			cout << "X: " << x << " Y: " << y << " Nivel: "<< nivelMaximoDeCinza <<endl;
+
+			//alocando espaço na memória para o vetor que armazenará os pixels
+			//NOTA: otimizar essa operação para arrays em C++
+			pixels = (int *) malloc (sizeof(int) * x * y);		
+
+			//lendo o resto do arquivo para conseguir os pixels
+			for(counter = 0; counter < (x*y); counter++){
+				arquivo >> reader; //ler o binário e armazenar em reader
+				pixels[counter] = (int)reader; /*transformar o binário em inteiro e armazenar em pixels*/
+			}
+			
+			//setando atributos do objeto
+			setCaminho(caminho);
+			dimensoes.setDimensoes(x,y);
+			setComentario(comentario);
+			setIdentificador(id);
+			setNivelMaximoDeCinza(nivelMaximoDeCinza);
+			setPixels(pixels);
+	
+		}
+	}
+	catch (int e){
+		//em caso de qualquer erro, apresentar essa mensagem para o usuário
+		cout << "Desculpe, operacao falhou! Exception Nº " << e << endl;
+	}
+
+	//fecha o arquivo
+	arquivo.close();
+	
+}
+
+void salvar(){
+	//CONSTRUIR MÉTODO
+}
+
+void salvar(string nome, string caminho){
+	//CONSTRUIR MÉTODO
+}
+
+void setNivelMaximoDeCinza(int nivelMaximoDeCinza){
+	this->nivelMaximoDeCinza = nivelMaximoDeCinza;
+}
+
+int getNivelMaximoDeCinza(){
+	return nivelMaximoDeCinza;
+}
+
+void setComentario(string comentario){
+	this->comentario = comentario;
+}
+
+string getComentario(){
+	return comentario;
+}
+
