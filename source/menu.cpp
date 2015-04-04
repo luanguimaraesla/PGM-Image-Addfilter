999218694795210a89bd36f9109b5133c70a2d21
Luan Guimarães <luan@live.in>
2015-04-04 19:36:20 -0300

    Implementando header de menu.hpp.

---
+++ source/menu.cpp
@@ -0,0 +1,108 @@
+#include "menu.hpp"
+#include "filtronegativo.hpp"
+#include "filtrosmooth.hpp"
+#include "filtrosharpen.hpp"
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+Menu::Menu(){
+
+}
+
+void Menu::iniciar(){
+	ImagemPGM copia;
+	
+	cout << "--------BEM VINDO AO APLICADOR DE FILTROS PGM-------" <<endl;
+
+	imagem.abrirImagem(menuCaminho());
+	aplicarUmFiltro(menuFiltros(), imagem, copia);
+}
+
+void Menu::aplicarUmFiltro(int opcao, ImagemPGM &imagem, ImagemPGM &copia){
+	FiltroNegativo fnegativo;
+	FiltroSharpen fsharpen;
+	FiltroSmooth fsmooth;
+
+	switch(opcao){
+		case 1:
+			fnegativo.aplicarFiltroEmCopia(imagem, copia);
+			break;
+		case 2:
+			fsmooth.aplicarFiltroEmCopia(imagem, copia);
+			break;
+		case 3:
+			fsharpen.aplicarFiltroEmCopia(imagem, copia);
+			break;
+		default:
+			cout << "Tchau!" <<endl;
+			exit(0);
+	}
+	
+	salvarImagem(menuSalvar(), imagem, copia);
+}
+
+int Menu::salvarImagem(int opcao, ImagemPGM &imagem, ImagemPGM &copia){
+	switch(opcao){
+		case 1:
+			copia.salvar();
+			break;
+		case 2:
+			copia.salvar(menuCaminho());
+			break;
+		case 3:
+			copia.salvar(menuCaminho());
+			aplicarUmFiltro(menuFiltros(), this->imagem, copia);
+			break;
+		case 4:
+			aplicarUmFiltro(menuFiltros(), copia, copia);		
+	}
+
+	cout << "Tchau!" <<endl;
+	exit(0);
+}
+
+int Menu::menuSalvar(){
+	cout <<endl;	
+	cout << "Selecione uma opção de salvamento: " <<endl;
+	cout <<endl;
+	cout << "0. Sair sem salvar.\n1. Sobrescrever arquivo original.\n2. Salvar em outro caminho.\n3. Salvar e testar outros filtros.\n4. Aplicar mais filtros a esta imagem antes de salvar.\n\nEscolha: "<<endl;
+	
+	return lerOpcao(0,4);
+}
+
+	
+
+string Menu::menuCaminho(){
+	string caminho;	
+	
+	cout <<endl;
+	cout <<"Digite o caminho do arquivo: ";
+	cin >> caminho;
+	
+	return caminho;
+}
+
+int Menu::menuFiltros(){
+	cout <<endl;
+	cout << "-------------------MENU DE FILTROS------------------" <<endl;
+	cout <<endl;
+	cout << "0. Sair\n1. Aplicar Negativo\n2. Aplicar Smooth\n3. Aplicar Sharpen"<<endl;
+	
+	return lerOpcao(0,3);
+}
+
+int Menu::lerOpcao(int limiteInferior, int limiteSuperior){
+	int opcao;
+	
+	cout <<endl;
+	cout << "Escolha: ";
+	cin >> opcao;
+	while(opcao > limiteSuperior || opcao < limiteInferior){
+		cout << "Erro, essa alternativa nao existe! Tente de novo: ";
+		cin >> opcao;
+	}
+	
+	return opcao;
+}
